406e9ed7f4d787726541113afc7a5a29662e2860
cameron <smokeice593@gmail.com>
2021-09-13 14:20:30 -0400

    Initial commit

---
+++ src/main/java/org/example/Largestnumber.java
@@ -0,0 +1,40 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 21 Solution
+ *  Copyright 2021 Cameron Parrish
+ */
+
+package org.example;
+import java.util.Scanner;
+
+
+public class Largestnumber {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int first = input.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int second = input.nextInt();
+
+        System.out.print("Enter the third number: ");
+        int third = input.nextInt();
+
+        // If none of the numbers equal eachother
+        if (first != second && second != third && first != third){
+            if (first > second && first > third){
+                System.out.printf("The largest number is %d.", first);
+            }
+
+            else if (second > first && second > third){
+                System.out.printf("The largest number is %d.", second);
+            }
+
+            else if (third > first && third > second){
+                System.out.printf("The largest number is %d.", third);
+            }
+        }
+
+    }
+}
